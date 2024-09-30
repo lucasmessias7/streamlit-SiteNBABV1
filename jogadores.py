f6141e6ac14c9824ca815c78c71f30162550295c
Lucas Messias <luquinhasmessias7@gmail.com>
2024-09-30 13:05:18 -0300

    ultima att-site

--- jogadores.py
+++ jogadores.py
@@ -2,13 +2,13 @@ import pandas as pd
 import streamlit as st
 import plotly
 
-dados_jogadores = pd.read_csv('dados/dados_jogadores_23-24.csv', delimiter=';')
 st.title('Dados jogadores')
 
+dados_jogadores = pd.read_csv('dados/dados_jogadores_23-24.csv', delimiter=';')
 
 
 pontos = dados_jogadores[[
-     'Jogador',
+    'Jogador',
     'Equipe',
     'Minutos jogados',
     'Taxa de acerto total',
@@ -17,12 +17,17 @@ pontos = dados_jogadores[[
     'Taxa de acerto de 3 pontos',
     'Pontos por jogo'
 ]]
+
+
 rebotes = dados_jogadores[[
     'Jogador',
     'Equipe',
     'Minutos jogados',
     'Rebotes'
 ]]
+
+
+
 assistencias = dados_jogadores[[
     'Jogador',
     'Equipe',
@@ -31,15 +36,10 @@ assistencias = dados_jogadores[[
 ]]
 
 
-paginas = ['all', 'Pontos', 'Rebotes', 'Assistencias']
-st.sidebar.title('Filtro')
-dados = st.sidebar.selectbox('Selecione o filtro:', paginas)
+filtro = ['Pontos','Rebotes', 'Assistencias']
 
-if dados == 'all':
-    dados = dados_jogadores
+st.sidebar.selectbox('NBA', filtro)
 
-
-    
 
 
 aba1, aba2, aba3 = st.tabs(['Pontos', 'Rebotes', 'Assistencias'])
