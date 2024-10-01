68bb190d7424f8729a82bda8bbe14e83372d53a3
Lucas Messias <luquinhasmessias7@gmail.com>
2024-10-01 11:47:48 -0300

    adicionado a função de pesquisa

--- jogadores.py
+++ jogadores.py
@@ -6,8 +6,20 @@ import plotly
 dados_jogadores = pd.read_csv('dados/dados_jogadores_23-24.csv', delimiter=';')
 
 
-def pesquisa():
-    st.text_input('Digite sua pesquisa: ')
+
+def faz_busca(df):
+    pesquisa = st.text_input('Digite sua pesquisa:')
+    if pesquisa:
+        dados_filtrados = df[df.apply(lambda row: row.astype(str).str.contains(pesquisa, case=False).any(), axis=1)]
+        st.write(f"Resultados para '{pesquisa}':")
+        st.dataframe(dados_filtrados)
+    else:
+        st.write("Digite algo na barra de pesquisa para ver os resultados.")
+
+    
+
+
+
 
 pontos = dados_jogadores[[
     'Jogador',
@@ -38,25 +50,25 @@ assistencias = dados_jogadores[[
 ]]
 
 
-filtro = ['Tudo','Pontos','Rebotes', 'Assistencias']
+filtro = ['Pontos','Rebotes', 'Assistencias']
 
 filtrado = st.sidebar.selectbox('NBA', filtro)
 
-if filtrado == 'Tudo':
-    st.title('Todos os dados dos jogadores')
-    dados_jogadores
 
 if filtrado == 'Pontos':
     st.title('Pontos por Jogador')
+    faz_busca(pontos)
     pontos
 
 
 if filtrado == 'Rebotes':
     st.title('Rebotes por jogador')
+    faz_busca(rebotes)
     rebotes
 
 if filtrado == 'Assistencias':
     st.title('Assistência por jogador')
+    faz_busca(assistencias)
     assistencias
 # aba1, aba2, aba3 = st.tabs(['Pontos', 'Rebotes', 'Assistencias'])
 
--- pages/times.py
+++ pages/times.py
@@ -2,8 +2,80 @@ import pandas as pd
 import streamlit as st
 import plotly
 
-st.title('Dados dos Times na temporada 23-24')
 
 dados_times = pd.read_csv('dados/dados_times_23-24.csv', delimiter=';')
+team_team = pd.read_csv('dados/teamVSteam_23-24.csv', delimiter=';')
 
-st.dataframe(dados_times)
+dados_times = dados_times.rename(columns={
+    'Unnamed: 0' : 'Posição',
+    'Unnamed: 1' : 'Time',
+    'Unnamed: 2' : 'Geral',
+    'Vitorias' : 'Em casa',
+    'Vitorias.1' : 'Fora de casa',
+    'Conference' : 'Contra Leste',
+    'Conferencia' : 'Contra Oeste'
+}).drop(columns=['Mês','Mês.1','Mês.2','Mês.3','Mês.4','Mês.5','Mês.6'])
+
+team_team = team_team.rename(columns={'ATL' : 'Atlanta Hawks',
+    'BOS' : 'Boston Celtics',
+    'BRK' : 'Brooklyn Nets',
+    'CHI' : 'Chicago Bulls',
+    'CHO' : 'Charlotte Hornets',
+    'CLE' : 'Cleveland Cavaliers',
+    'DAL' : 'Dallas Mavericks',
+    'DEN' : 'Denver Nuggets',
+    'DET' : 'Detroit Pistons',
+    'GSW' : 'Golden State Warriors',
+    'HOU' : 'Houston Rockets',
+    'IND' : 'Indiana Pacers',
+    'LAC' : 'Los Angeles Clippers',
+    'LAL' : 'Los Angeles Lakers',
+    'MEM' : 'Memphis Grizzlies',
+    'MIA' : 'Miami Heat',
+    'MIL' : 'Milwaukee Buckes',
+    'MIN' : 'Minnesota Timberwolves',
+    'NOP' : 'New Orleans Pelicans',
+    'NYK' : 'New York Knicks',
+    'OCK' : 'Oklahoma City Thunder',
+    'ORL' : 'Orlando Magic',
+    'PHI' : 'Philadelphia 76ers',
+    'PHO' : 'Phoenix Suns',
+    'POR' : 'Portland Trail Blazers',
+    'SAC' : 'Sacramento Kings',
+    'SAS' : 'San Antonio Spurs',
+    'TOR' : 'Toronto Raptors',
+    'UTA' : 'Utah Jazz',
+    'WAS' : 'Washington Wizards'})
+
+
+
+
+
+def busca_time(df):
+    pesquisa = st.text_input('Digite o  nome do time:')
+    if pesquisa:
+        dados_filtrados = df[df.apply(lambda row: row.astype(str).str.contains(pesquisa, case=False).any(), axis=1)]
+        st.write(f"Resultados para '{pesquisa}':")
+        st.dataframe(dados_filtrados)
+    else:
+        st.write("Digite algo na barra de pesquisa para ver os resultados.")
+
+
+
+st.title('Dados dos Times na temporada 23-24')
+
+
+filtro_time = ['Confrontos','Vitorias']
+filtrado_time = st.sidebar.selectbox('Dados', filtro_time)
+
+
+
+if filtrado_time == 'Confrontos':
+    st.title('Confrontos')
+    busca_time(team_team)
+
+
+
+if filtrado_time == 'Vitorias':
+    st.title('Vitórias e derrotas ')
+    busca_time(dados_times)
