60025afa5797a8544c0f0b1382f789a6719cf0c3
Lucas Messias <luquinhasmessias7@gmail.com>
2024-11-06 10:35:39 -0300

    atualizacao de dados

---
+++ dados.py
@@ -0,0 +1,88 @@
+import json
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+
+with open('dados_tabela.json', 'r', encoding='utf-8') as f:
+    dados_json = json.load(f)
+df = pd.DataFrame(dados_json)
+df = df.rename(columns= {
+                        0 : 'Jogador',
+                        1 : 'idade',
+                        2 : 'Equipe',
+                        3 : 'Posição',
+                        4 : 'Jogos',
+                        5 : 'Jogos Iniciados',
+                        6 : 'Minutos Jogados',
+                        7 : 'Cestas totais',
+                        8 : 'Cestas tentadas totais',
+                        9 : 'Porcentagem de cestas',
+                        10 : 'Cestas de 3 pontos',
+                        11 : 'Tentativas de cestas de 3',
+                        12 : 'Porcentagem de cestas de 3 pontos',
+                        13 : 'Cestas de 2 pontos',
+                        14 : 'Tentativas de cestas de 2 pontos',
+                        15 : 'Porcentagem de cestas de 2 pontos',
+                        16 : 'EFG%',
+                        17 : 'Lances livres',
+                        18 : 'Tentativas de lances livres',
+                        19 : 'Porcentagem de lances livres',
+                        20 : 'Rebotes ofensivos',
+                        21 : 'Rebotes defensivos',
+                        22 : 'Rebotes totais',
+                        23 : 'Assistências',
+                        24 : 'Roubos de bola',
+                        25 : 'Bloqueios',
+                        26 : 'Perdas de bola',
+                        27 : 'Faltas pessoais',
+                        28 : 'Pontos'
+})
+
+def faz_busca(df):
+    pesquisa = st.text_input('Digite sua pesquisa:')
+    if pesquisa:
+        dados_filtrados = df[df.apply(lambda row: row.astype(str).str.contains(pesquisa, case=False).any(), axis=1)]
+        st.write(f"Resultados para '{pesquisa}':")
+        st.dataframe(dados_filtrados)
+    else:
+        st.write("Digite algo na barra de pesquisa para ver os resultados.")
+
+
+rebotes = df[[
+    'Jogador',
+    'Equipe',
+    'Minutos Jogados',
+    'Rebotes ofensivos',
+    'Rebotes defensivos',
+    'Rebotes totais'
+]]
+
+
+
+assistencias = df[[
+    'Jogador',
+    'Equipe',
+    'Minutos Jogados',
+    'Assistências'
+]]
+
+
+filtro = ['Dados gerais','Rebotes', 'Assistencias']
+filtrado = st.sidebar.selectbox('NBA', filtro)
+
+
+if filtrado == 'Dados gerais':
+    st.title('Dados gerais')
+    faz_busca(df)
+    df
+
+if filtrado == 'Rebotes':
+    st.title('Rebotes por jogador')
+    faz_busca(rebotes)
+    rebotes
+
+if filtrado == 'Assistencias':
+    st.title('Assistência por jogador')
+    faz_busca(assistencias)
+    assistencias
